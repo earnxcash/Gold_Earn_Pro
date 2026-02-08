d6c1656cafbaa1e767046dc7ca29accf8fd959ec
earnxcash <strangerstatesusa@gmail.com>
2026-02-08 12:15:59 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000, // Explicitly run on port 3000
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000', // Backend URL
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  build: {
+    rollupOptions: {
+      external: [
+        '@capacitor-community/admob',
+        '@capacitor/core',
+        '@capacitor/network'
+      ]
+    }
+  }
 });
